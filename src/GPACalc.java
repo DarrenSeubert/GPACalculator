eea5a8fb9da194f5f0c606483ba26b10e1446c61
Darren Seubert <dpseubert15@gmail.com>
2025-10-06 22:10:21 -0500

    Updated total credits wording

--- src/GPACalc.java
+++ src/GPACalc.java
@@ -29,7 +29,7 @@ public class GPACalc {
     Scanner scnr = new Scanner(System.in);
     
     do { // Total Credits Prior to Semester Prompt
-      System.out.println("Enter Total Credits (GPA Units) Prior to this Semester:");
+      System.out.println("Enter Total GPA Credits (GPA Units) Prior to this Semester:");
       if (scnr.hasNextInt()) {
         totalPreviousCredits = scnr.nextInt();
 
